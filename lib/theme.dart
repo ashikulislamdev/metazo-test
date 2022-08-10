ac6277eed9f36d37eb8df3ab99b6f11be7d535e0
saun2648 <ashikulislamsawan@gmail.com>
2022-08-10 23:06:10 +0600

    collect color, add package and remove unnecessary path

---
+++ lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {  //this file is collected from internet
+  static const whiteGrey = Color(0xFFE5E5E5);
+  static const pink = Color(0xFFCE125C);
+  static const fullWhite = Color(0xFFFFFFFF);
+  static const normalPink = Color(0xFFFF4AC2);
+  static const anotherPink = Color(0xFFB3167E);
+  static const textBlack = Color(0xFF000000);
+  static const textGreyHard = Color(0xFF464646);
+  static const textGrey = Color(0xFF7C7C7C);
+  static const textGreen = Color(0xFF0BD01F);
+  static const red = Color(0xFFFF0202);
+  static const blue = Color(0xFF0093FF);
+}
